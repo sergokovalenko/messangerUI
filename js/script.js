69e0576915a59288343528c3892c0f01bc4d8cc2
Sergey Kovalenko <kovalenkosergey.98@yandex.ru>
2018-02-16 19:58:17 +0400

    Changed <a> on <div> in dialogs and messages Added correct counter of new mess. position

--- js/script.js
+++ js/script.js
@@ -2,8 +2,16 @@ $("document").ready(function () {
     var a = $('.scroll-pane');
     a.jScrollPane({
         maintainPosition: true,
-        stickToBottom: true
+        stickToBottom: true,
+        contentWidth: 200
     });
 
     a.data("jsp").scrollTo(0,1000);
 })
+
+window.onresize = function(){
+    $('.scroll-pane').jScrollPane({
+        maintainPosition: true,
+        stickToBottom: true
+    });
+}
