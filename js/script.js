e47a4d300093a1f84023278a1cc13800e37c6c12
Sergey Kovalenko <kovalenkosergey.98@yandex.ru>
2018-05-15 22:34:47 +0400

    Reconstruction
    -renamed class
    -added classes and ids
    -added namespaces
    -added the first part of buttons

--- js/script.js
+++ js/script.js
@@ -1,3 +1,45 @@
+var APP = APP || {};
+APP.createNamespace = function (path) {
+    var parts = path.split('.'),
+        parent = APP,
+        i = 0;
+
+    if (parts[i] === 'APP') {
+        parts = parts.slice(1);
+    }
+
+    for (i = 0; i < parts.length; i += 1) {
+        if (typeof parent[parts[i]] === "undefined") {
+            parent[parts[i]] = {};
+        }
+
+        parent = parent[parts[i]];
+    }
+
+    return parent;
+}
+
+APP.createNamespace('APP.models.buttons');
+APP.createNamespace('APP.models.fields');
+APP.createNamespace('APP.models.identeties');
+APP.createNamespace('APP.utilities.actions');
+
+APP.models.buttons = {
+    $btnSettings: $('.settings-btn'),
+    $btnSearch: $('.search-btn'),
+    $btnCreateConvers: $('.create-conversation'),
+    $btnSendMess: $('#send-message'),
+    $btnSendPict: $('#send-picture'),
+    $btnRusLang: $('.dropdown-menu li:nth-child(1)'),
+    $btnRusLang: $('.dropdown-menu li:nth-child(2)'),
+};
+
+APP.models.fields = {
+    $sendField: $('.send-field'),
+    $searchField: $('.search-field input'),
+};
+
+
 $("document").ready(function () {
     var a = $('.scroll-pane');
     a.jScrollPane({
@@ -6,10 +48,10 @@ $("document").ready(function () {
         contentWidth: 200
     });
 
-    a.data("jsp").scrollTo(0,1000);
+    a.data("jsp").scrollTo(0, 1000);
 })
 
-window.onresize = function(){
+window.onresize = function () {
     $('.scroll-pane').jScrollPane({
         maintainPosition: true,
         stickToBottom: true
