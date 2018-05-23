1d8d0b4f9b5a5027bc36d08066aa35f5bb3b7c54
Sergey Kovalenko <kovalenkosergey.98@yandex.ru>
2018-05-23 21:24:26 +0400

    new update
    little changes in html
    deleted useless file
    added new functions in js file

--- js/script.js
+++ js/script.js
@@ -27,6 +27,7 @@ APP.createNamespace('APP.utilities.actions');
 APP.createNamespace('APP.utilities.ajax');
 
 APP.models.entities = {
+    me: { id: 100, login: "BestGuy", url: "img/profiles/my.jpg", name: "Sergey", surname: "Kovalenko", regDate: "6/10/1998", status: "Online", email: "best@people.math" },
     dialogs: [{ id: 1, url: "img/profiles/my.jpg", name: "ivan", surname: "ivanov", lastMessage: "last msg", date: "9:05PM", unreaded: 5 },
     { id: 2, url: "img/profiles/my.jpg", name: "vova", surname: "ivanov", lastMessage: "last msg", date: "Yestarday", unreaded: 5 },
     { id: 3, url: "img/profiles/my.jpg", name: "vasya", surname: "ivanov", lastMessage: "last mess", date: "9:00 PM", unreaded: 5 },
@@ -54,12 +55,17 @@ APP.models.fields = {
 
 APP.utilities.actions = (function () {
     var dialogs = APP.models.entities.dialogs,
-        profiles = APP.models.entities.profiles;
+        profiles = APP.models.entities.profiles,
+        me = APP.models.entities.me,
+        buttons = APP.models.buttons;
+    fields = APP.models.fields;
 
     function showDialogs() {
         var $form = $('.jspPane:eq(0)'),
             $names = {},
+            $dialogs = {},
             html = '',
+            i = 0,
             elem = {};
 
         //получение диалогов из ajax
@@ -76,34 +82,64 @@ APP.utilities.actions = (function () {
         });
 
         //работа с данными
-        for (let i = 0; i < dialogs.length; i++) {
+        for (i = 0; i < dialogs.length; i++) {
             elem = dialogs[i];
 
             html += '<div class="dialog"><img class="profile-photo" src="' + elem.url + '" alt="user">' +
                 '<a class="dial-name">' + elem.name + ' ' + elem.surname + '</a>' +
                 '<span class="last-message-time">' + elem.date + '</span>' +
                 '<div class="short-message">' + elem.lastMessage + '</div>' +
-                '<span class="badge">'+elem.unreaded+'</span></div>';
+                '<span class="badge">' + elem.unreaded + '</span></div>';
         }
 
-        $form.html(html);;
+        $form.html(html);
 
         $names = $('.dial-name');
+        $dialogs = $('.dialog');
 
-        for (let i = 0; i < $names.length; i++) {
+        for (i = 0; i < $names.length; i++) {
+            $dialogs[i].current = i;
+            $dialogs[i].onclick = openDialog;
+
             $names[i].current = i;
-            $names[i].onclick = showModal;
+            $names[i].onclick = function (e) {
+                e.stopPropagation();
+                showModal(dialogs[this.current].id);
+            };
         }
     }
 
-    function showModal() {
-        var html = "", i = this.current,
-            $modalBody = $('.modal-body'),
+    function openDialog() {
+        var data = { currentUserId: me.id, showId: dialogs[this.current].id }
+
+        $.ajax({
+            url: "GetMessage",
+            data: JSON.stringify(data),
+            success: function (request) {
+                var res = JSON.parse(request);
+                dialogs = res;
+                //дальнейшая работа
+            },
+            error: function (e) {
+                // console.log(e);
+            }
+        });
+
+        $('.dialog').removeClass('activeted');
+        $(this).addClass('activeted');
+
+        alert('show dialog for id: ' + dialogs[this.current].id);
+    }
+
+    function showModal(id) {
+        var html = "", i = id,
+            user = {};
+        $modalBody = $('.modal-body'),
             $modalFooter = $('.modal-footer');
 
         $.ajax({
             url: "GetProfileInfo",
-            data: JSON.stringify(i),
+            data: JSON.stringify(id),
             success: function (request) {
                 var res = JSON.parse(request);
                 //дальнейшая работа
@@ -113,20 +149,29 @@ APP.utilities.actions = (function () {
             }
         });
 
+        if (i != 100) {
+            user = profiles[i];
+        }
+        else {
+            user = me;
+        }
+
         html = '<div class="row"><div class="col-xs-5"><img class="profile-img" src="img/profiles/my.jpg" alt="user photo"></div>' +
-            '<div class="col-xs-7"><div class="name text-center">' + profiles[i].name + ' ' + profiles[i].surname + '</div>' +
-            '<div class="status text-center" >' + profiles[i].status + '</div>' +
+            '<div class="col-xs-7"><div class="name text-center">' + user.name + ' ' + user.surname + '</div>' +
+            '<div class="status text-center" >' + user.status + '</div>' +
             '<button type="button" class="btn btn-default btn-write" data-dismiss="modal">Write</button></div></div></div>';
 
         $modalBody.html(html);
 
         html = '<div class="row"><div class="col-xs-5"><h4 class="profile-info text-right">E-mail:</h4>' +
             '<h4 class="profile-info text-right">Login:</h4><h4 class="profile-info text-right">Registration:</h4></div>' +
-            '<div class="col-xs-7"><h4 class="profile-info text-left">' + profiles[i].email + '</h4>' +
-            '<h4 class="profile-info text-left">' + profiles[i].login + '</h4>' +
-            '<h4 class="profile-info text-left">' + profiles[i].regDate + '</h4 ></div ></div >';
+            '<div class="col-xs-7"><h4 class="profile-info text-left">' + user.email + '</h4>' +
+            '<h4 class="profile-info text-left">' + user.login + '</h4>' +
+            '<h4 class="profile-info text-left">' + user.regDate + '</h4 ></div ></div >';
 
         $modalFooter.html(html);
+
+        $('.btn-write').on('click', openDialog);
 
         $('#Modal').modal('show');
     }
@@ -149,9 +194,42 @@ APP.utilities.actions = (function () {
         }
     }
 
+    function initializeSearch() {
+        fields.$searchField.on('blur', function () {
+            var value = this.val();
+
+            //получение данных на основании фильтра
+            $.ajax({
+                url: "GetDialogs",
+                data: JSON.stringify(value),
+                success: function (request) {
+                    var res = JSON.parse(request);
+                    //дальнейшая работа
+                },
+                error: function (e) {
+                    console.log(e);
+                }
+            });
+        });
+    }
+
+    function initialization() {
+        buttons.$btnSearch.on('click', function () {
+            fields.$searchField.focus();
+            fields.$searchField.val("");
+        });
+
+        buttons.$btnSettings.on('click', function () {
+            showModal(me.id);
+        });
+
+        initializeScroll();
+        // initializeSearch();
+    }
+
     return {
         showDialogs: showDialogs,
-        initializeScroll: initializeScroll,
+        initialization: initialization,
     }
 })();
 
@@ -159,6 +237,6 @@ APP.utilities.actions = (function () {
 $("document").ready(function () {
     var actions = APP.utilities.actions;
 
-    actions.initializeScroll();
+    actions.initialization();
     actions.showDialogs();
 });
